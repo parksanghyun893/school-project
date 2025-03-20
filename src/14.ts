0a22fe938ae0ad428c696c8ea0bcc9c5cbee0a4b
parksanghyun893 <parksanghyun893@gmail.com>
2025-03-20 03:38:50 +0000

    Add 14.ts

---
+++ src/14.ts
@@ -0,0 +1,17 @@
+ 
+// School-Project - TS Code
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-school-project',
+  templateUrl: './school-project.component.html',
+  styleUrls: ['./school-project.component.css']
+})
+export class SchoolProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
