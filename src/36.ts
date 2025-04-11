0d8a3911be4493952ed96bbf1d62a27c2d95ad3a
parksanghyun893 <parksanghyun893@gmail.com>
2025-04-11 12:39:10 +0000

    Add 36.ts

---
+++ src/36.ts
@@ -0,0 +1,17 @@
+// Example of basic operations such as addition, subtraction, multiplication, division
+
+// Addition 
+let result1 = 5 + 3;
+console.log(result1);
+
+// Subtraction 
+let result2 = 8 - 4;
+console.log(result2);
+
+// Multiplication 
+let result3 = 9 * 7;
+console.log(result3);
+
+// Division
+let result4 = 16 / 4;
+console.log(result4);
