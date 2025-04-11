eb278d8cfcd872177db4a1280df420d6d1cbeafb
parksanghyun893 <parksanghyun893@gmail.com>
2025-04-11 13:00:54 +0000

    Add 48.ts

---
+++ src/48.ts
@@ -0,0 +1,7 @@
+// This is a very simple example demonstrating basic operations with arrays.
+// You can modify it to perform more complex tasks as needed.
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+numbers.forEach((element) => {
+  console.log(element);
+});
