b1d0cd4c13fa07b54609836f5f0517e649d01d5d
parksanghyun893 <parksanghyun893@gmail.com>
2025-04-11 12:58:00 +0000

    Add 46.ts

---
+++ src/46.ts
@@ -0,0 +1,8 @@
+let num = 5;
+if(num > 0) {
+    console.log("Number is greater than zero.");
+} else if(num == 0) {
+    console.log("Number is zero.");
+} else {
+    console.log("Number is less than or equal to zero.");
+}
